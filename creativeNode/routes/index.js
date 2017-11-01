fc491b3a55b0db6a4dabc7148d2c636c3969097f
Tyler Grant <tyler.grant.2010@gmail.com>
2017-11-01 01:41:44 +0000

    Back end

--- creativeNode/routes/index.js
+++ creativeNode/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var names = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/names', function(req, res) {
+  console.log("In getnames");
+  res.send(names);
+});
+
+router.post('/names', function(req, res) {
+    console.log("In getnames post");
+    console.log(req.body);
+    var name = req.query.name;
+	request('https://api.genderize.io/?name=' + name, function(error, response, body){
+		names.push(req.body);
+		res.send(body);
+	})
+}); 
 
 module.exports = router;
